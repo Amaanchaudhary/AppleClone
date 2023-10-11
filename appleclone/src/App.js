cd2d0d1c7455382b77d67dfc452e04b641a97b5e
Amaanchaudhary <chaudharyamaan982@gmail.com>
2023-10-11 21:23:54 +0530

    update

--- appleclone/src/App.js
+++ appleclone/src/App.js
@@ -1,14 +1,20 @@
 import { Route , Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Components/HomePage'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage/>} />
+        <Route path='/navbar' element={<Navbar/>} />
+        <Route path='/footer' element={<Footer/>} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
