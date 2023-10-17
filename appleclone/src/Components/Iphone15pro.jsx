f7713ff094b62b8b3c2153246075dcdae88926ae
Amaanchaudhary <chaudharyamaan982@gmail.com>
2023-10-17 23:43:50 +0530

    update

--- appleclone/src/Components/Iphone15pro.jsx
+++ appleclone/src/Components/Iphone15pro.jsx
@@ -7,7 +7,30 @@ const Iphone15pro = () => {
                 <p>Save up to ₹6000.00 instantly on iPhone with HDFC Bank Credit Cards. Plus No Cost EMI from most leading banks.‡ <span>Shop iphone</span></p>
             </div>
             <div className='apple15-body1'>
+                <div className='apple15-image1'>
                     <p>iPhone 15 Pro</p>
+                </div>
+                <button className='apple15-button1'>Buy</button>
+                <p className='apple15-div1-P'>From ₹21483.00/mo.‡ or ₹134900.00*</p>
+            </div>
+            <div className='apple15-body2'>
+                <div className='apple15-body2-heading'>
+                    <h1>Get the highlights.</h1>
+                    <div>
+                        <a>Watch the film <i class="fa-regular fa-circle-play"></i></a>
+                        <a>Watch the event <i class="fa-solid fa-chevron-right fa-xs"></i></a>
+                    </div>
+                </div>
+                <div className='apple15-body2-image'>
+                    <p style={{ marginBlockStart: '40px' }}>Enter A17 Pro.</p>
+                    <p>Game-changing chip.</p>
+                    <p>Groundbreaking performance.</p>
+                </div>
+            </div>
+            <div className='apple15-body3'>
+                <h1>Take a closer look.</h1>
+                <div>   </div>
+                <p>6.7” iPhone 15 Pro Max1 and 6.1” iPhone 15 Pro1 in Blue Titanium</p>
             </div>
         </div>
     )
